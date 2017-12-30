64d75fc29d32652f8145c544ee71c5d1e250cbe2
L kabeer <ahmed@Lkabeer.com>
2017-12-30 13:48:36 +0200

    Firebase Settings in Angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9So210jCG-T8dMyWs5xZ9ghaZjoFk_84",
+  authDomain: "simple-angular-4-app-x-team.firebaseapp.com",
+  databaseURL: "https://simple-angular-4-app-x-team.firebaseio.com",
+  projectId: "simple-angular-4-app-x-team",
+  storageBucket: "simple-angular-4-app-x-team.appspot.com",
+  messagingSenderId: "916445040459"
+};
 
 
 @NgModule({
@@ -12,7 +24,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
